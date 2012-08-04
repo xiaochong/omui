99149c377a20b570ffa6c3aa01e9f15b28e7629b
groovyquan <groovyquan@gmail.com>
2012-08-04 22:23:30 +0800

    i18n support

--- grails-app/conf/OmuiPluginResources.groovy
+++ grails-app/conf/OmuiPluginResources.groovy
@@ -16,7 +16,24 @@ modules = {
         resource url: [plugin: 'omui', dir: 'operamasks-ui/js', file: "operamasks-ui.min.js"], nominify: true
     }
 
+    'operamasks-ui-lang-en-us' {
+        resource url: [plugin: 'omui', dir: 'operamasks-ui/lang', file: "en-us.js"]
+    }
+
+    'operamasks-ui-lang-zh-tw' {
+        resource url: [plugin: 'omui', dir: 'operamasks-ui/lang', file: "zh-tw.js"]
+    }
+
     'operamasks-ui' {
+        dependsOn 'operamasks-ui-jquery,operamasks-ui-js,operamasks-ui-lang-en-us,operamasks-ui-theme-apusic'
+    }
+
+    'operamasks-ui-zh-cn' {
         dependsOn 'operamasks-ui-jquery,operamasks-ui-js,operamasks-ui-theme-apusic'
     }
+
+    'operamasks-ui-zh-tw' {
+        dependsOn 'operamasks-ui-jquery,operamasks-ui-js,operamasks-ui-lang-zh-tw,operamasks-ui-theme-apusic'
+    }
+
 }
