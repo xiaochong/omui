1d4e797e451e924d21b6748062a89149ba4e855f
groovyquan <groovyquan@gmail.com>
2012-08-04 17:58:04 +0800

    readme

--- OmuiGrailsPlugin.groovy
+++ OmuiGrailsPlugin.groovy
@@ -13,9 +13,9 @@ class OmuiGrailsPlugin {
     // TODO Fill in these fields
     def title = "Operamasks UI Plugin" // Headline display name of the plugin
     def author = "groovyquan"
-    def authorEmail = ""
+    def authorEmail = "groovyquan[at]gmail[dot]com"
     def description = '''\
-Brief summary/description of the plugin.
+This plugin supplies Taglib same as zkui plugin to simplify OperaMasks UI(http://ui.operamasks.org).
 '''
 
     // URL to the plugin's documentation
@@ -24,7 +24,7 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
@@ -33,10 +33,10 @@ Brief summary/description of the plugin.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [system: "github", url: "https://github.com/xiaochong/omui/issues"]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [url: "https://github.com/xiaochong/omui/"]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
---
+++ README.md
@@ -0,0 +1,6 @@
+### Introduction
+This plugin supplies Taglib same as zkui plugin to simplify OperaMasks UI(http://ui.operamasks.org).
+
+
+
+
